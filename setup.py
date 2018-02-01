c188a1a4c82e88575106bc4b8dd7b4ab2cb2e0e6
Jakob Stemberger <Jakob.Stemberger@booxware.de>
2018-02-01 20:38:23 +0100

    Removed necissity for importing ytools.ytools. Introduced single source for version.

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-version = '0.8.2'
+from ytools import __version__
+
 here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
   long_description = f.read()
@@ -13,14 +14,14 @@ setup(
   name = 'ytools',
   packages = find_packages(),
   install_requires = ['PyYaml', 'jsonpath-ng', 'jsonschema'],
-  version = '%s' % version,
+  version = '%s' % __version__,
   description = 'Command-line tool for selectively dumping nodes from `yaml` (or `json`) documents in `yaml` or `json` format.',
   long_description=long_description,
   author = 'Jakob Stemberger',
   author_email = 'yaccob@gmx.net',
   license = license,
   url = 'https://github.com/yaccob/ytools', # use the URL to the github repo
-  download_url = 'https://github.com/yaccob/ytools/archive/%s.tar.gz' % version, # I'll explain this in a second
+  download_url = 'https://github.com/yaccob/ytools/archive/%s.tar.gz' % __version__, # I'll explain this in a second
   keywords = ['yaml', 'json', 'python', 'jsonpath', 'json-path', 'dump', 'convert', 'validate', 'schema'],
   classifiers = ['Programming Language :: Python :: 2.7'],
   scripts = ['scripts/ytools'],
---
+++ ytools/__init__.py
@@ -0,0 +1,3 @@
+from ytools import *
+
+__version__ = '0.8.3'
