dc687a63ac30b2f8b2dd6f9ac4e1993abea5ccdb
Jakob Stemberger <Jakob.Stemberger@booxware.de>
2018-05-25 23:45:52 +0200

    Committing for Version 0.9.2

--- ytools/ytools.py
+++ ytools/ytools.py
@@ -11,10 +11,10 @@ import jsonschema
 
 optiondefaults = {"yaml": "{explicit_start: True, explicit_end: True, allow_unicode: True}", "json": "{indent: 2, encoding: utf-8}", "python": "{}"}
 
-def validate(schemafile, args, encoding='utf-8'):
+def validate(schemafile, datafiles, encoding='utf-8'):
     schema = yaml.load("".join(fileinput.input(schemafile)))
-    documents = yaml.load_all("".join(fileinput.input(args, openhook=fileinput.hook_encoded(encoding))))
-    for filename in args:
+    documents = yaml.load_all("".join(fileinput.input(datafiles, openhook=fileinput.hook_encoded(encoding))))
+    for filename in datafiles:
         with open(filename) as infile:
             for document in yaml.load_all(infile):
                 try:
@@ -23,7 +23,7 @@ def validate(schemafile, args, encoding='utf-8'):
                     e.filename = filename
                     raise e
 
-def dump(args, path='$', format='yaml', yaml_options=optiondefaults['yaml'], json_options=optiondefaults['json'], encoding='utf-8'):
+def dump(datafiles, path='$', format='yaml', yaml_options=optiondefaults['yaml'], json_options=optiondefaults['json'], encoding='utf-8'):
     def orderedDict_constructor(loader, node, deep=False):
         data = collections.OrderedDict()
         yield data
@@ -39,7 +39,7 @@ def dump(args, path='$', format='yaml', yaml_options=optiondefaults['yaml'], jso
     yaml.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, encoders[format]["yaml_constructor"])
     yaml.add_constructor(u'tag:yaml.org,2002:timestamp', yaml.constructor.SafeConstructor.construct_yaml_str)
     yaml.add_representer(collections.OrderedDict, lambda dumper, data: dumper.represent_dict(data.iteritems()))
-    documents = yaml.load_all("".join(fileinput.input(args, openhook=fileinput.hook_encoded(encoding))))
+    documents = yaml.load_all("".join(fileinput.input(datafiles, openhook=fileinput.hook_encoded(encoding))))
     formatoptions = dict(yaml.load(optiondefaults[format]), **yaml.load(encoders[format]["kwargs"]))
     for document in documents:
         for match in jsonpath.parse(path).find(document):
