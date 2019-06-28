99dbe4da021a923fcb71b87399e53d6a004da651
yaccob <yaccob@gmx.net>
2019-06-28 17:58:52 -0400

    Added `Loader` parameter to yaml.load calls

--- ytools/__init__.py
+++ ytools/__init__.py
@@ -1,3 +1,3 @@
 from ytools import *
 
-__version__ = '0.9.7'
+__version__ = '0.9.8'
--- ytools/ytools.py
+++ ytools/ytools.py
@@ -12,11 +12,11 @@ import jsonschema
 optiondefaults = {"yaml": "{explicit_start: True, explicit_end: True, allow_unicode: True}", "json": "{indent: 2, encoding: utf-8}", "python": "{}"}
 
 def validate(schemafile, datafiles, encoding='utf-8'):
-    schema = yaml.load("".join(fileinput.input(schemafile)))
-    documents = yaml.load_all("".join(fileinput.input(datafiles, openhook=fileinput.hook_encoded(encoding))))
+    schema = yaml.load("".join(fileinput.input(schemafile)), Loader=yaml.FullLoader)
+    documents = yaml.load_all("".join(fileinput.input(datafiles, openhook=fileinput.hook_encoded(encoding))), Loader=yaml.FullLoader)
     for filename in datafiles:
         with open(filename) as infile:
-            for document in yaml.load_all(infile):
+            for document in yaml.load_all(infile, Loader=yaml.FullLoader):
                 try:
                     jsonschema.validate(document, schema, format_checker=jsonschema.FormatChecker())
                 except jsonschema.exceptions.ValidationError, e:
@@ -39,8 +39,8 @@ def dump(datafiles, path='$', format='yaml', yaml_options=optiondefaults['yaml']
     yaml.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, encoders[format]["yaml_constructor"])
     yaml.add_constructor(u'tag:yaml.org,2002:timestamp', yaml.constructor.SafeConstructor.construct_yaml_str)
     yaml.add_representer(collections.OrderedDict, lambda dumper, data: dumper.represent_dict(data.iteritems()))
-    documents = yaml.load_all("".join(fileinput.input(datafiles, openhook=fileinput.hook_encoded(encoding))))
-    formatoptions = dict(yaml.load(optiondefaults[format]), **yaml.load(encoders[format]["kwargs"]))
+    documents = yaml.load_all("".join(fileinput.input(datafiles, openhook=fileinput.hook_encoded(encoding))), Loader=yaml.FullLoader)
+    formatoptions = dict(yaml.load(optiondefaults[format], Loader=yaml.FullLoader), **yaml.load(encoders[format]["kwargs"], Loader=yaml.FullLoader))
     for document in documents:
         for match in jsonpath.parse(path).find(document):
             print encoders[format]["dumper"](match.value, **formatoptions)
